2920bd5b54e378025b6025630aaaf70898a2859b
kbt <kubota@softdevice.co.jp>
2019-08-27 18:03:25 +0900

    no message

--- game/ball.lua
+++ game/ball.lua
@@ -11,12 +11,31 @@ function Ball:update(dt, players, playerGoals)
   local x = self.x + dt * math.cos(self.direction) * self.speed
   local y = self.y + dt * math.sin(self.direction) * self.speed
 
+  if x < -1.2 or x > 1.2 or y > 1.2 or y < -1.2 then
+    self.x = 0
+    self.y = 0
+    return
+  end
+
   -- player との衝突判定
   for _, player in ipairs(players) do
     local rx, ry, cx, cy = player.lineCollision:reflect(self.x, self.y, x, y)
 
     -- 衝突検知
     if rx ~= nil then
+
+--[[
+      local rx2, ry2, cx2, cy2 = player.lineCollision:reflect(cx+(rx-cy)*1, cy+(ry-cy)*1, rx, ry)
+      if rx2~=nil then rx, ry, cx, cy = rx2, ry2, cx2, cy2 end
+      local goalIndex = checkGoals(playerGoals, cx+(rx-cy)*0.01, cy+(ry-cy)*0.01, rx, ry)
+      if goalIndex ~= nil then
+        self:goCenter()
+        players[goalIndex]:death()
+        self.musicSystem:play(players[goalIndex].index, "death")
+        return
+      end
+--]]
+
       self.x, self.y = rx, ry
       self.direction = math.atan2(ry - cy, rx - cx)
       if math.random(1, 10) < 3 then
@@ -29,21 +48,31 @@ function Ball:update(dt, players, playerGoals)
   end
 
   -- player の取りこぼし衝突判定
-  for index, goal in ipairs(playerGoals) do
-    local cx, cy = goal:intersection(self.x, self.y, x, y)
-
-    -- 衝突検知
-    if cx ~= nil then
-      self.x, self.y = 0, 0
-      self.direction = love.math.random() * math.pi * 2
-
-      self.musicSystem:play(players[index].index, "death")
-      players[index]:death()
-      return
-    end
+  local goalIndex = checkGoals(playerGoals, self.x, self.y, x, y)
+  if goalIndex ~= nil then
+    self:goCenter()
+    players[goalIndex]:death()
+    self.musicSystem:play(players[goalIndex].index, "death")
+    return
   end
 
   self.x, self.y = x, y
+end
+
+function Ball:goCenter()
+  self.x, self.y = 0, 0
+  self.direction = love.math.random() * math.pi * 2
+end
+
+function checkGoals(playerGoals, x, y, nextX, nextY)
+  for index, goal in ipairs(playerGoals) do
+    local cx, cy = goal:intersection(x, y, nextX, nextY)
+    -- 衝突検知
+    if cx ~= nil then
+      return index
+    end
+  end
+  return nil
 end
 
 function Ball:draw()
